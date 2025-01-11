9dc977fd35b134d39d524075cb22699f61f8cf1d
omid <o.shali01@gmail.com>
2025-01-11 18:06:27 +0330

    add router for api

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const debug = require("debug")("app:main");
 const config = require("config");
 
+const router = require("./src/routes/index");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +15,8 @@ mongoose
   .connect(config.get("db.address"))
   .then(() => debug("connected to mongodb"))
   .catch(() => debug("could not connected to mongodb"));
+
+app.use("/api", router);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
