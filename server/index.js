75b296d00f9a5f1490586fc70f912b6799de2def
omid <o.shali01@gmail.com>
2025-01-12 18:22:52 +0330

    install corse and require that

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 const debug = require("debug")("app:main");
 const config = require("config");
